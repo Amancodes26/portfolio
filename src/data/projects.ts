e58cd0c87ca4c5144745a1a25c1ec97ddc30c1bd
Amancodes26 <amansngh8586@gmail.com>
2025-01-19 17:28:55 +0530

    project updated

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,33 +1,36 @@
 export const projects = [
-    {
-      name: '   WEB DEV IDE',
-      desc: 'WEB DEV IDE is a open source platform which connects user to build their own application using HTML ,CSS ,javascript',
-      liveLink: 'https://online-code-editior-6kbv.vercel.app/',
-      githubLink: 'https://github.com/Amancodes26/ONLINE-CODE-EDITIOR/tree/main',
-      coverImg: '/projects/dfgb.png',
-      imgs: [],
-      tech: [
-        '/logos/typescript.svg',
-        '/logos/nextjs-13.svg',
-        '/logos/react.svg',
-        '/logos/shadcn.jpeg',
-        '/logos/tailwind.svg',
-        '/logos/zod.svg',
-      ],
-    },
-    {
-      name: 'Aman Portfolio',
-      desc: 'The website you are currently on. I built this site to showcase my skills and my portfolio',
-      liveLink: 'https://portfolio-amancodes26s-projects.vercel.app/',
-      githubLink: 'https://github.com/Amancodes26/portfolio',
-      coverImg: '/projects/portfolio.png',
-      imgs: [],
-      tech: [
-        '/logos/typescript.svg',
-        '/logos/nextjs-13.svg',
-        '/logos/react.svg',
-        '/logos/tailwind.svg',
-      ],
-    },
-   
-  ];
+  {
+    name: "FrameFrusion",
+    desc: "FrameFrusion Generate AI Image From Text",
+    liveLink: "https://framefusionai.vercel.app/",
+    githubLink: "https://github.com/Amancodes26/FrameFusion",
+    coverImg: "/projects/dwefgg.png",
+    imgs: [],
+    tech: [
+      "/logos/typescript.svg",
+      "/logos/nextjs-13.svg",
+      "/logos/react.svg",
+      "/logos/shadcn.jpeg",
+      "/logos/tailwind.svg",
+      "/logos/zod.svg",
+    ],
+  },
+
+  {
+    name: " WEB DEV IDE",
+    desc: "WEB DEV IDE is a open source platform which connects user to build their own application using HTML ,CSS ,javascript",
+    liveLink: "https://online-code-editior-6kbv.vercel.app/",
+    githubLink: "https://github.com/Amancodes26/ONLINE-CODE-EDITIOR/tree/main",
+    coverImg: "/projects/dfgb.png",
+    imgs: [],
+    tech: [
+      "/logos/typescript.svg",
+      "/logos/nextjs-13.svg",
+      "/logos/react.svg",
+      "/logos/shadcn.jpeg",
+      "/logos/tailwind.svg",
+      "/logos/zod.svg",
+    ],
+  },
+  
+];
